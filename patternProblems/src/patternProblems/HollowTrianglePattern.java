f2b6c054d41c405b361b9b8fbc338b8146732992
AkhilT-HB <akhilhb02@gmail.com>
2024-08-01 12:15:41 +0530

    Added modified hollow triangle pattern

--- patternProblems/src/patternProblems/HollowTrianglePattern.java
+++ patternProblems/src/patternProblems/HollowTrianglePattern.java
@@ -8,22 +8,16 @@ public class HollowTrianglePattern {
 		for(int i=1;i<=n;i++) {
 			
 			for(int j=1;j<=(n-i);j++) {
-				System.out.print("+");
+				System.out.print(" ");
 			}
 			
-			System.out.print("*");
-			
-			
-			if(i>1) {
-				for(int j=1;j<i;j++) {
-					System.out.print("+");
+			for(int k=1;k<= ((2*i)-1);k++) {
+				if(k==1||i==n||k==((2*i)-1)) {
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
 				}
-				System.out.print("*");
 			}
-			
-//			for(int j=1;j<=i;j++) {
-//				System.out.print("* ");
-//			}
 			
 			System.out.println();
 			
