4cbb5a386531950e4ac7c434c448349bb921f858
AkhilT-HB <akhilhb02@gmail.com>
2024-11-21 15:18:34 +0530

    LeetCodeRemoveDuplicatesFromSortedArray Problem

--- ProblemSolving/src/GFGProblems_TwoPointers/LeetCodeRemoveDuplicatesFromSortedArray.java
+++ ProblemSolving/src/GFGProblems_TwoPointers/LeetCodeRemoveDuplicatesFromSortedArray.java
@@ -1,35 +1,46 @@
 package GFGProblems_TwoPointers;
+/*
+Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
 
-import java.util.ArrayList;
-import java.util.List;
+Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+
+Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+Return k.
+*/
+
+import java.util.Arrays;
 
 public class LeetCodeRemoveDuplicatesFromSortedArray {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] nums = {1,1,2};
+		int[] nums = {0,0,1,1,1,2,2,3,3,4};
 		
-		removeDuplicates(nums);
+		
+		
+		System.out.println(removeDuplicates(nums));
 	}
 
 	public static int removeDuplicates(int[] nums) {
 
-		List<Integer> temp = new ArrayList<>();
-		int size = 0;
-
-		for (int i = 0; i < nums.length-1; i++) {
-			if (nums[i] != nums[i + 1]) {
-				temp.add(nums[i]);
-				size++;
+		int n=nums.length;
+		int i=0;//to keep the position of the element that needs to be compared
+		int j=1;//to loop and find the element which is not equal to i
+		int k=1;//to keep the position that needs to be updated when an unequal element is found
+		
+		while(j<n) {
+			if(nums[i]!=nums[j]) {
+				nums[k]=nums[j];
+				i++;
+				k++;
+			}else {
+				j++;
 			}
 		}
 		
-		temp.add(nums[nums.length-1]);
+		System.out.println(Arrays.toString(nums));
 		
-		System.out.println(temp);
-		
-		
-		return temp.size();
+		return k;
 	}
 
 }
