2ffe46d81d8fe2296f397a291c0bb4c7d387ccdc
AkhilT-HB <akhilhb02@gmail.com>
2025-02-19 19:19:55 +0530

    DSA practice 2D arrays

--- DSAPractice/src/Arrays/Main.java
+++ DSAPractice/src/Arrays/Main.java
@@ -24,9 +24,18 @@ public class Main {
 		
 		TwoDimensionalArray tda = new TwoDimensionalArray(3, 3);
 		tda.insertValueInTheArray(0, 0, 10);
-		tda.insertValueInTheArray(0, 0, 20);
+		tda.insertValueInTheArray(0, 1, 20);
+		tda.insertValueInTheArray(1, 0, 30);
+		tda.insertValueInTheArray(2, 0, 40);
+		
+		//tda.accessCell(0, 1);
 		System.out.println(Arrays.deepToString(tda.arr));
 		
+		//tda.traverse2DArray();
+		
+		//tda.searchValue(50);
+		tda.deleteValueFromArray(0, 0);
+		System.out.println(Arrays.deepToString(tda.arr));
 	}
 
 }
--- DSAPractice/src/Arrays/TwoDimensionalArray.java
+++ DSAPractice/src/Arrays/TwoDimensionalArray.java
@@ -13,7 +13,7 @@ public class TwoDimensionalArray {
 				arr[row][col] = Integer.MIN_VALUE;
 			}
 		}
-		System.out.println(Arrays.deepToString(arr));
+		//System.out.println(Arrays.deepToString(arr));
 	}
 	
 	//Inserting Values in 2D array
@@ -27,6 +27,50 @@ public class TwoDimensionalArray {
 			}
 		}catch(ArrayIndexOutOfBoundsException e) {
 			System.out.println("The index is not valid for 2D array");
+		}
+	}
+	
+	//Accessing cell value of the array
+	public void accessCell(int row, int column) {
+		System.out.println("\nAccessing row#"+row+", col#"+column);
+		try {
+			System.out.println("Cell value is "+arr[row][column]);
+		}catch(ArrayIndexOutOfBoundsException e){
+			System.out.println("The index is not valid for 2D array");
+		}
+	}
+	
+	//Traversing 2D array
+	public void traverse2DArray() {
+		for(int row=0;row<arr.length;row++) {
+			for(int col=0;col<arr[row].length;col++) {
+				System.out.print(arr[row][col]+" ");
+			}
+			System.out.println();
+		}
+	}
+	
+	//Searching a single value from array
+	public void searchValue(int value) {
+		for(int row=0;row<arr.length;row++) {
+			for(int col=0;col<arr[row].length;col++) {
+				if(arr[row][col]==value) {
+					System.out.println("The value is in arr["+row+"]["+col+"]");
+					return;
+				}
+			}
+		}
+		
+		System.out.println("Value is not found");
+	}
+	
+	//Deleting a value from 2D array
+	public void deleteValueFromArray(int row, int col) {
+		try {
+			System.out.println("Successfully deleted: "+ arr[row][col]);
+			arr[row][col]=Integer.MIN_VALUE;
+		}catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("This index is not valid for array");
 		}
 	}
 
