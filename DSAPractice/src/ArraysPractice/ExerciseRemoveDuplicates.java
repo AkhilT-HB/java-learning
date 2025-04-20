1e86c4dd8122c3863249e4798e02875524f97f44
AkhilT-HB <akhilhb02@gmail.com>
2025-04-20 19:15:59 +0530

    Added RemoveDuplicates exercise

---
+++ DSAPractice/src/ArraysPractice/ExerciseRemoveDuplicates.java
@@ -0,0 +1,30 @@
+package ArraysPractice;
+
+public class ExerciseRemoveDuplicates {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] nums = {1,1,1,2,2,3};
+		
+		System.out.println(removeDuplicates(nums));
+	}
+
+	public static int removeDuplicates(int[] nums) {
+		// TODO
+		if(nums.length==0) {
+			return 0; 
+		}
+		
+		int i = 0;
+		
+		for(int j=1;j<nums.length;j++) {
+			if(nums[j]!= nums[i]) {
+				i++;
+				nums[i]=nums[j];
+			}
+		}
+		
+		return i+1;
+	}
+
+}
