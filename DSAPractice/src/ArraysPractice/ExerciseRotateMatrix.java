c1d82afee98ed90b7e435184d2fbfff5732952eb
AkhilT-HB <akhilhb02@gmail.com>
2025-04-25 00:48:11 +0530

    Adding Exercise Rotate Matrix

---
+++ DSAPractice/src/ArraysPractice/ExerciseRotateMatrix.java
@@ -0,0 +1,30 @@
+package ArraysPractice;
+
+/*
+Rotate Matrix
+Given an image represented by an NxN matrix write a method to rotate the image by 90 degrees.
+
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly.
+
+DO NOT allocate another 2D matrix and do the rotation.
+ 
+*/
+
+public class ExerciseRotateMatrix {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void rotateMatrix(int[][] matrix) {
+		
+		if(matrix.length == matrix[0].length) {
+			
+			
+			
+		}
+
+	}
+
+}
