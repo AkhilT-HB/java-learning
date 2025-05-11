1c44cbf5c668d94717e497df1c5bfbe145e35c61
AkhilT-HB <akhilhb02@gmail.com>
2025-05-11 13:49:29 +0530

    Adding exercise Rotate Matrix with solution

--- DSAPractice/src/ArraysPractice/ExerciseRotateMatrix.java
+++ DSAPractice/src/ArraysPractice/ExerciseRotateMatrix.java
@@ -1,5 +1,7 @@
 package ArraysPractice;
 
+import java.util.Arrays;
+
 /*
 Rotate Matrix
 Given an image represented by an NxN matrix write a method to rotate the image by 90 degrees.
@@ -14,16 +16,41 @@ public class ExerciseRotateMatrix {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
+		
+		int[][] matrix =  {{5,1,9,11},{2,4,8,10},{13,3,6,7},{15,14,12,16}};
+		
+		System.out.println(Arrays.deepToString(matrix));
+		
+		rotateMatrix(matrix);
+		
+		System.out.println(Arrays.deepToString(matrix));
 
 	}
 
-	public static void rotateMatrix(int[][] matrix) {
+	public static boolean rotateMatrix(int[][] matrix) {
 		
-		if(matrix.length == matrix[0].length) {
-			
-			
-			
+		if(matrix.length==0 || (matrix.length != matrix[0].length)) return false;
+		
+		int n = matrix.length;
+		
+		for(int layer=0;layer<n/2;layer++) {
+			int first=layer;
+			int last=n-1-layer;
+			for(int i=first;i<last;i++) {
+				int offset = i-first;
+				System.out.println("layer is: "+layer+" first is: "+first+" last is: "+last+" offset is: "+offset);
+
+				int top = matrix[first][i];
+				matrix[first][i]=matrix[last-offset][first];
+				matrix[last-offset][first]=matrix[last][last-offset];
+				matrix[last][last-offset]=matrix[i][last];
+				matrix[i][last]=top;
+			}
 		}
+
+		
+		return true;
 
 	}
 
