c7f1e736c53aac36ce0bc281b9d9f744a58a75a4
AkhilT-HB <akhilhb02@gmail.com>
2025-08-24 16:11:21 +0530

    Practice prime numbers efficient ways

---
+++ DSAPractice/src/Maths/FindingKthPrimeNumber.java
@@ -0,0 +1,66 @@
+package Maths;
+
+import java.util.ArrayList;
+
+/*
+ * Input
+An integer stating the number of queries Q (equal to 50000), and Q lines follow, each containing one integer K between 1 and 5000000 inclusive.
+
+Output
+Q lines with the answer of each query: the Kth prime number.
+
+Example:
+Input:
+7
+1
+10
+100
+1000
+10000
+100000
+1000000
+
+Output:
+2
+29
+541
+7919
+104729
+1299709
+15485863
+ */
+
+public class FindingKthPrimeNumber {
+	
+	static int N = 86028121;
+	static boolean[] sieve = new boolean[N+1];
+	
+	static int createSieve(int k) {
+		for(int i=2;i<=N;i++) {
+			sieve[i]=true;
+		}
+		
+		for(int i=2;i*i<=N;i++) {
+			if(sieve[i]==true) {
+				for(int j=i*i;j<=N;j+=i) {
+					sieve[j]=false;
+				}
+			}
+		}
+		
+		ArrayList<Integer> list = new ArrayList<>();
+		for(int i = 2;i<=N;i++) {
+			if(sieve[i]==true) {
+				list.add(i);
+			}
+		}
+		
+		return list.get(k);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(createSieve(5));
+	}
+
+}
---
+++ DSAPractice/src/Maths/PrimeNumbersSeiveMethod.java
@@ -0,0 +1,40 @@
+package Maths;
+
+import java.util.Arrays;
+
+public class PrimeNumbersSeiveMethod {
+	
+	static boolean createSieve(int n) {
+		boolean[] sieve = new boolean[n+1];
+		
+		for(int i=0;i<=n;i++) {
+			sieve[i]=true;
+		}
+		
+		for(int i=2;i*i<=n;i++) {
+			if(sieve[i]==true) {
+				for(int j=i*i;j<=n;j+=i) {
+					sieve[j] = false;
+				}
+			}
+		}
+		if(sieve[n]==true) return true;
+		
+		return false;
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int n = 1000003;
+		
+		if(createSieve(n)==true) {
+			System.out.println("YES");
+		}else {
+			System.out.println("NO");
+		}
+
+	}
+
+}
