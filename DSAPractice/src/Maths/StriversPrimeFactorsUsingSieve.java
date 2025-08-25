76f2f5f40ae951ed6868ada72b32a3942dde0270
AkhilT-HB <akhilhb02@gmail.com>
2025-08-25 20:25:47 +0530

    Added - StriversPrimeFactorsUsingSieve

---
+++ DSAPractice/src/Maths/StriversPrimeFactorsUsingSieve.java
@@ -0,0 +1,37 @@
+package Maths;
+
+public class StriversPrimeFactorsUsingSieve {
+	
+	static int N = 1000000;
+	static int[] sieve = new int[N+1];
+	static void createSieve() {
+		for(int i=0;i<=N;i++) {
+			sieve[i]=i;
+		}
+		
+		for(int i=2;i*i<=N;i++) {
+			if(sieve[i]==i) {
+				for(int j=i*i;j<=N;j+=i) {
+					if(sieve[j]==j) {
+						sieve[j]=i;
+					}
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		createSieve();
+		
+		int k = 36;
+
+		
+		while(k!=1) {
+			System.out.println(sieve[k]);
+			k=k/sieve[k];
+		}
+
+	}
+
+}
