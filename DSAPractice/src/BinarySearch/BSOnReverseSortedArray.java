1e211f410c31713cb47632309d39822ec407aa4e
AkhilT-HB <akhilhb02@gmail.com>
2025-09-16 16:17:06 +0530

    Binary Search learning - Reverse sorted Array

---
+++ DSAPractice/src/BinarySearch/BSOnReverseSortedArray.java
@@ -0,0 +1,29 @@
+package BinarySearch;
+
+public class BSOnReverseSortedArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = {20,17,15,14,13,12,10,9,8,4};
+		
+		int start = 0;
+		int end = arr.length - 1;
+		
+		int target = 4;
+		
+		while(start<=end) {
+			int mid = start + (end-start / 2);
+			if(arr[mid] == target) {
+				System.out.println(mid);
+				break;
+			}else if (target > arr[mid]) {
+				end = mid - 1;
+			}else {
+				start = mid + 1;
+			}
+			
+		}
+
+	}
+
+}
