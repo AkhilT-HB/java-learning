300456d6e271f690f72829a5dba0ffc9baf3a54b
AkhilT-HB <akhilhb02@gmail.com>
2025-09-17 18:25:48 +0530

    Binary Search - Firat and Last occurence of an element

---
+++ DSAPractice/src/BinarySearch/FirstOccurrenceOfAnElement.java
@@ -0,0 +1,32 @@
+package BinarySearch;
+
+public class FirstOccurrenceOfAnElement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] arr = {2,4,10,10,10,18,20};
+		
+		int start = 0;
+		int end = arr.length-1;
+		int target = 10;
+		
+		int result = -1;
+		
+		while(start<=end) {
+			int mid = start + ((end - start)/2);
+			
+			if(arr[mid] == target) {
+				result = mid;
+				end = mid - 1;
+			}else if(target<mid) {
+				end = mid - 1;
+			}else {
+				start = mid + 1;
+			}
+		}
+		
+		System.out.println(result);
+	}
+
+}
---
+++ DSAPractice/src/BinarySearch/LastOccurenceOfAnElement.java
@@ -0,0 +1,31 @@
+package BinarySearch;
+
+public class LastOccurenceOfAnElement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = {2,4,10,10,10,18,20};
+		
+		int start = 0;
+		int end = arr.length-1;
+		int target = 10;
+		
+		int result = -1;
+		
+		while(start<=end) {
+			int mid = start + ((end - start)/2);
+			
+			if(arr[mid]==target) {
+				result = mid;
+				start = mid + 1;
+			}else if(target<arr[mid]) {
+				end = mid - 1;
+			}else {
+				start = start + 1;
+			}
+		}
+		
+		System.out.println(result);
+	}
+
+}
