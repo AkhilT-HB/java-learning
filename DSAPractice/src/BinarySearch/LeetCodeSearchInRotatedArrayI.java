ff19c7b8f05b44e71703fcd625ae4fd170d28ca4
AkhilT-HB <akhilhb02@gmail.com>
2025-09-18 23:57:27 +0530

    Binary Search - LeetCode SearchInRotatedArrayI

---
+++ DSAPractice/src/BinarySearch/LeetCodeSearchInRotatedArrayI.java
@@ -0,0 +1,43 @@
+package binarySearch;
+
+public class LeetCodeSearchInRotatedArrayI {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] nums = {4,5,6,7,0,1,2};
+		int target = 3;
+		
+		System.out.println(search(nums, target));
+
+	}
+	 public static int search(int[] nums, int target) {
+
+	        int start = 0;
+	        int end = nums.length - 1;
+
+	        while(start<=end){
+	            int mid = start + ((end-start)/2);
+
+	            if(nums[mid]==target){
+	                return mid;
+	            }else if(nums[start]<=nums[mid]){
+	                if(nums[start] <= target && target <= nums[mid]){
+	                    end = mid - 1;
+	                }else{
+	                    start = mid + 1;
+	                }
+	            }else if(nums[mid]<=nums[end]){
+	                if(nums[mid] <= target && target <= nums[end]){
+	                    start = mid + 1;
+	                }else{
+	                    end = mid - 1;
+	                }
+	            }
+	        }
+
+	        return -1;
+	        
+	    }
+
+}
