8699b26b91b7ce856b7b5ca29302634a052a6ed6
AkhilT-HB <akhilhb02@gmail.com>
2025-09-25 03:00:18 +0530

    BruteForceMaximumSubArraySizeHavingAllSubArraysSumLessThanK

---
+++ DSAPractice/src/BinarySearch/BruteForceMaximumSubArraySizeHavingAllSubArraysSumLessThanK.java
@@ -0,0 +1,31 @@
+package binarySearch;
+
+public class BruteForceMaximumSubArraySizeHavingAllSubArraysSumLessThanK {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = {1, 2, 3, 4};
+        int k = 8;
+        System.out.println(maxSubarraySize(arr, k));
+	}
+
+	private static int maxSubarraySize(int[] arr, int k) {
+		int n  = arr.length;
+		int ans = -1;
+		for(int i = 1;i <= n;i++) {
+			int maxSum = Integer.MIN_VALUE;
+			for(int j = 0;j < n-i+1;j++) {
+				int sum = 0;
+				for(int l = j;l < j + i;l++) {
+					sum += arr[l];
+				}
+				maxSum = Math.max(maxSum, sum);
+			}
+			if(maxSum<=k) {
+				ans =  Math.max(ans, i);
+			}
+		}
+		return ans;
+	}
+
+}
