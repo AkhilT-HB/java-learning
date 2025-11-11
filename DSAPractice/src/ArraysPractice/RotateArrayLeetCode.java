acd060a303c89e81b8a89b1a0b22d08b6d85e604
AkhilT-HB <akhilhb02@gmail.com>
2025-11-11 23:45:00 +0530

    RotateArray

---
+++ DSAPractice/src/ArraysPractice/RotateArrayLeetCode.java
@@ -0,0 +1,38 @@
+package arraysPractice;
+
+public class RotateArrayLeetCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] nums = {-1,-100,3,99};
+		int k = 2;
+		
+		rotate(nums,k);
+		
+		for(int i = 0;i<nums.length;i++) {
+			System.out.println(nums[i]);
+		}
+	}
+	public static void rotate(int[] nums, int k) {
+		int[] subArrayB = new int[k];
+        int j = subArrayB.length-1;
+        for(int i = nums.length-1;i>=k;i++){
+            subArrayB[j--] = nums[i];
+        }
+
+        int[] subArrayA = new int[k+1];
+        for(int i = 0;i<=k;i++){
+            subArrayA[i] = nums[i];
+        }
+
+        for(int i = 0;i<k;i++){
+            nums[i] = subArrayB[i];
+        }
+
+        int start = 0;
+        for(int i = k;i<nums.length;i++){
+            nums[i] = subArrayA[start];
+            start++;
+        }
+    }
+}
