4e519264a65840efb1b9c5581c73a939459a77c3
AkhilT-HB <akhilhb02@gmail.com>
2025-11-20 23:13:20 +0530

    Modified MajorityElementII

--- DSAPractice/src/ArraysPractice/MajorityElementII_LeetCode.java
+++ DSAPractice/src/ArraysPractice/MajorityElementII_LeetCode.java
@@ -1,6 +1,7 @@
 package arraysPractice;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -29,6 +30,46 @@ public class MajorityElementII_LeetCode {
         }
 
         return list.stream().distinct().collect(Collectors.toList());
+    }
+	
+	public static List<Integer> majorityElementBoyerMooreMethod(int[] nums) {
+        int n = nums.length;
+
+        if(n==0) return Collections.emptyList();
+
+        int cand1 = 0, cand2 = 0;
+        int count1 = 0, count2 =0;
+
+        for(int num : nums){
+            if(num == cand1){
+                count1++;
+            }else if(num == cand2){
+                count2++;
+            }else if(count1 == 0){
+                cand1 = num;
+                count1 = 1;
+            }else if(count2 == 0){
+                cand2 = num;
+                count2 = 1;
+            }else{
+                count1--;
+                count2--;
+            }
+        }
+
+        count1 = 0;
+        count2 = 0;
+
+        for(int num : nums){
+            if(num==cand1) count1++;
+            else if(num==cand2) count2++;
+        }
+
+        List<Integer> list = new ArrayList<>();
+        if(count1>n/3) list.add(cand1);
+        if(count2>n/3) list.add(cand2);
+
+        return list;
     }
 
 }
