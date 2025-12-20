9c68f8329b991ffb45f4c6e4dda4a0e58532cfa4
AkhilT-HB <akhilhb02@gmail.com>
2025-12-20 23:52:40 +0530

    Power Of 2

---
+++ DSAPractice/src/recursionAndbacktracking/PowerOf2LeetCode.java
@@ -0,0 +1,21 @@
+package recursionAndbacktracking;
+
+public class PowerOf2LeetCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 12;
+		System.out.println(isPower2(n));
+	}
+	
+	static boolean isPower2(int n) {
+		if(n<=0) return false;
+		
+		if(n==1) return true;
+		
+		if(n%2 != 0) return false;
+		
+		return isPower2(n/2);
+	}
+
+}
