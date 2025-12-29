4e6ea4c5b32fb40081b414e2791b81609d4c3cd7
AkhilT-HB <akhilhb02@gmail.com>
2025-12-30 00:31:40 +0530

    Updated Generate parenthese

--- DSAPractice/src/recursionAndbacktracking/GenerateParenthese.java
+++ DSAPractice/src/recursionAndbacktracking/GenerateParenthese.java
@@ -7,7 +7,7 @@ public class GenerateParenthese {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int n = 1;
+		int n = 3;
 		System.out.println(generateParenthesis(n));
 	}
 	
